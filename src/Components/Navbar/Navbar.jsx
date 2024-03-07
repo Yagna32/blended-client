0d7297933399ee7e473fa29732e9ceb4bc003856
yagna <yagnapatelhirenk@gmail.com>
2024-03-08 01:08:47 +0530

    Asking for login even if there is valid tokens fixed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef} from 'react'
+import React, { useContext, useEffect, useRef, useState} from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import './Navbar.css'
 import logo from '../Assets/logo-b.png'
@@ -6,13 +6,22 @@ import cart_icon from '../Assets/cart_icon.png'
 import { ShopContext } from '../../Context/ShopContext'
 import nav_dropdown from '../Assets/nav_dropdown.png'
 const Navbar = () => {
-    const {getTotalCartItems,menu,activateMenu} = useContext(ShopContext)
+    const {getTotalCartItems,menu,setMenu,checkTokens} = useContext(ShopContext)
+    const [isLoggedIn,setIsLoggedIn] = useState(false);
     const menuRef = useRef();
 
     const dropdown_toggle = (e) =>{
         menuRef.current.classList.toggle('nav-menu-visible');
         e.target.classList.toggle('open');
     }
+
+    useEffect(()=>{
+        const checkAuth = async()=>{
+            const result = await checkTokens();
+            setIsLoggedIn(result);
+        }
+        checkAuth()
+    },[checkTokens])
 
   return (
     <div className='navbar'>
@@ -23,15 +32,15 @@ const Navbar = () => {
         </div>
         <img className='nav-dropdown'src={nav_dropdown} onClick={dropdown_toggle} alt="" />
         <ul ref={menuRef} className='nav-menu'>
-            <li onClick={()=>{activateMenu("Shop")}}> <Link to='/' style={{textDecoration:'none'}}>Shop</Link>{menu==="Shop" ? <hr/>:undefined}</li>
-            <li onClick={()=>{activateMenu("Men")}}><Link to='/men' style={{textDecoration:'none'}}>Men</Link>{menu==="Men" ? <hr/>:undefined}</li>
-            <li onClick={()=>{activateMenu("Women")}}><Link to='/women' style={{textDecoration:'none'}}>Women</Link> {menu==="Women" ? <hr/>:undefined}</li>
-            <li onClick={()=>{activateMenu("Kids")}}><Link to='/kid' style={{textDecoration:'none'}}>Kids</Link>{menu==="Kids" ? <hr/>:undefined}</li>
+            <li onClick={()=>{setMenu("Shop")}}> <Link to='/' style={{textDecoration:'none'}}>Shop</Link>{menu==="Shop" ? <hr/>:undefined}</li>
+            <li onClick={()=>{setMenu("Men")}}><Link to='/men' style={{textDecoration:'none'}}>Men</Link>{menu==="Men" ? <hr/>:undefined}</li>
+            <li onClick={()=>{setMenu("Women")}}><Link to='/women' style={{textDecoration:'none'}}>Women</Link> {menu==="Women" ? <hr/>:undefined}</li>
+            <li onClick={()=>{setMenu("Kids")}}><Link to='/kid' style={{textDecoration:'none'}}>Kids</Link>{menu==="Kids" ? <hr/>:undefined}</li>
         </ul>
         <div className="nav-login-cart">
-            {localStorage.getItem('refresh-token')
-            ?<button onClick={()=>{localStorage.removeItem('access-token');localStorage.removeItem('refresh-token');window.location.replace('/')}}>Logout</button>
-            :<Link to='/Login'><button>Login</button></Link>
+            {isLoggedIn 
+                ?<button onClick={()=>{localStorage.removeItem('access-token');localStorage.removeItem('refresh-token');window.location.replace('/')}}>Logout</button>
+                :<Link to='/Login'><button>Login</button></Link>
             }
             <Link to="/cart"><img src={cart_icon} alt="" /></Link>
             
