45fdec6f030d0398e2bfbdf933f5589cf74402b4
yagna <yagnapatelhirenk@gmail.com>
2024-05-11 23:13:23 +0530

    added footer pages and after payment pages

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ import Footer from './Components/Footer/Footer';
 import men_banner from './Components/Assets/banner_mens.png'
 import women_banner from './Components/Assets/banner_women.png'
 import kid_banner from './Components/Assets/banner_kids.png'
+import OrderPage from './Pages/OrderPage';
+import Succes from './Pages/Succes';
+import Failed from './Pages/Failed';
+import ContactUs from './Pages/ContactUs';
+import AboutUs from './Pages/AboutUs';
+import Office from './Pages/Office';
+import News from './Pages/News';
 function App() {
   return (
     <div>
@@ -25,6 +32,13 @@ function App() {
             </Route>
             <Route path='/cart' element={<Cart />} />
             <Route path='/login' element={<LoginSignUp />} />
+          <Route path='/myorders' element={<OrderPage/>}/>
+          <Route path='/Successful' element={<Succes/>}/>
+          <Route path='/Failed' element={<Failed/>}/>
+          <Route path='/contactus' element={<ContactUs/>}/>
+          <Route path='/aboutus' element={<AboutUs/>}/>
+          <Route path='/offices' element={<Office/>}/>
+          <Route path='/News' element={<News/>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>
